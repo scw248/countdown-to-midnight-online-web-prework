617aa7da6d5e1e8e2bd93883fc61ac41bc94b1a0
Stephen Wright <scw248@gmail.com>
2019-01-03 15:34:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,15 @@ def countdown(num)
   "HAPPY NEW YEAR!"
 end
 
+
+
+def countdown_with_sleep(num)
+    
+    while num > 0
+    puts "#{num} SECONDS!"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+  
+end
